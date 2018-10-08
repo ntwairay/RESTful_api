e1fcb55806c16a0e23223ac6a1c3d283c0ecd1c8
Ray Ng <ray.ng@gradeous.com.au>
2018-10-09 10:52:03 +1100

    fix typo

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ var server = app.listen(port, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("API is listening at running")
+  console.log(`API is listening at ${port}`)
 
 })
